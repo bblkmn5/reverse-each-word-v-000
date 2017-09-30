157769c6a47a81cbfe685cd32fd78bac68f4f95c
Ben Blackman <bblkmn5@gmail.com>
2017-09-30 00:53:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(strings)
-  my_strings = []
-  strings.each do |string|
-    my_strings << string.split(", ")
+  strings.split.each do |word|
+    word.reverse
   end
-  puts my_strings.reverse!
+  word.join(" ")
 end
+  
